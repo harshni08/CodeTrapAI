1fa2425aaf85e37a6549913ef905e7dae5f2c6a7
harshni08 <harshniraj04@gmail.com>
2025-03-08 17:51:10 +0000

    Updated API endpoint in UI

--- ui.py
+++ ui.py
@@ -5,6 +5,6 @@ st.title("CodeTap AI")
 
 code = st.text_area("Enter your code:")
 if st.button("Check Code"):
-    response = requests.post("http://127.0.0.1:8000/predict/", json={"code": code})
+    response = requests.post("https://codetrapai.onrender.com/predict/", json={"code": code})
     result = response.json().get("result", "Error")
     st.success(f"Result: {result}")
